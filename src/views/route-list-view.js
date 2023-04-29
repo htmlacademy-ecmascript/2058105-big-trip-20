2b7660f9ec3dd0c8ffcde296c03c2df08de8adaa
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-04-29 22:17:57 +0300

    Отрисовка маршрутного листа

--- src/views/route-list-view.js
+++ src/views/route-list-view.js
@@ -1,24 +1,33 @@
 import View from './view.js';
-import {html} from '../utils.js';
+import CardView from './card-view.js';
 
 class RouteListView extends View {
   constructor() {
     super();
 
-    this.classList.add('route-list-view');
+    this.classList.add('trip-list');
+    this.setAttribute('role', 'list');
   }
 
   /**
    * @override
    */
-  createHtml() {
-    return html`
+  render() {
+    const views = new Array(4).fill().map(this.createItemView);
+    this.replaceChildren(...views);
+  }
 
-    `;
+  createItemView() {
+    const view = new CardView;
+    view.classList.add('trip-list__item');
+    view.setAttribute('role', 'listitem');
+    view.render();
+
+    return view;
   }
 }
 
-customElements.define('list-view', RouteListView);
+customElements.define('route-list-view', RouteListView);
 
 export default RouteListView;
 
